794c085240204bfe6f8ccaea55fc75baf86dc74d
powcoder <powcoder@163.com>
2022-12-03 19:46:13 +0800

    COMP9318 Project 2

---
+++ submission.py
@@ -0,0 +1,24 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVR
+from sklearn.metrics import mean_absolute_error
+
+
+## Project-Part1
+def predict_COVID_part1(svm_model, train_df, train_labels_df, past_cases_interval, past_weather_interval, test_feature):
+    pass ## Replace this line with your implementation
+
+
+
+## Project-Part2
+def predict_COVID_part2(train_df, train_labels_df, test_feature):
+    pass ## Replace this line with your implementation
+
